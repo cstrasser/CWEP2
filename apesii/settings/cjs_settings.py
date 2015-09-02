17e16c0a9c71796a7bc4c1d6ea0f4ec3d979e676
unknown <cstrasser@BRKITDT14-CS.newterra.local>
2015-09-02 15:59:51 -0400

    Completed settings for windows dev mahcine - now working

--- apesii/settings/cjs_settings.py
+++ apesii/settings/cjs_settings.py
@@ -10,3 +10,19 @@ DATABASES = {
     'PORT': '1433',
      }
 }
+
+STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK  = 'bootstrap3'
+#STATIC_ROOT =  "C:\users\cstrasser\documents\git_repo\apesii\apesii\"
+STATIC_ROOT = "c:\
+"
+#os.path.join(BASE_DIR, "static)
+print "static root:%s " %STATIC_ROOT
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+#STATIC_PATH = "C:\users\cstrasser\documents\git_repo\\apesii\\apesii\
+              
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+              
+print "Static Path: %s " %STATIC_PATH
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
