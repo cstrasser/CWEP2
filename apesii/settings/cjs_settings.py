94bad24b7cd3b92c526ed070f1f120e07834a724
unknown <cstrasser@BRKITDT14-CS.newterra.local>
2015-09-02 10:08:01 -0400

    Changed CJS settings for windows dev

--- apesii/settings/cjs_settings.py
+++ apesii/settings/cjs_settings.py
@@ -1,1 +1,12 @@
 from base import *
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'sql_server.pyodbc',
+    'NAME': 'APES_DJANGO',
+    'USER': DB_USER,
+    'PASSWORD': DB_PASS,
+    'HOST': '10.0.0.133',
+    'PORT': '1433',
+     }
+}
