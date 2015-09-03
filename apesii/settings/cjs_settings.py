d9752f5177bd86475e17dd08ef84abd9678c0313
unknown <cstrasser@BRKITDT14-CS.newterra.local>
2015-09-03 08:26:14 -0400

    new navpage to mimic existing

--- apesii/settings/cjs_settings.py
+++ apesii/settings/cjs_settings.py
@@ -13,15 +13,11 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK  = 'bootstrap3'
-#STATIC_ROOT =  "C:\users\cstrasser\documents\git_repo\apesii\apesii\"
-STATIC_ROOT = "c:\
-"
-#os.path.join(BASE_DIR, "static)
-print "static root:%s " %STATIC_ROOT
+#STATIC_ROOT =  os.path.join(BASE_DIR,'static') 
+
 STATIC_PATH = os.path.join(BASE_DIR,'static')
-#STATIC_PATH = "C:\users\cstrasser\documents\git_repo\\apesii\\apesii\
               
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static1"), )
               
 print "Static Path: %s " %STATIC_PATH
 MEDIA_URL = '/media/'
